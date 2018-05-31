14fc8414caf9584b6090a67620448f5beb4877df
Adam Dorsey <adam.dorsey@gmail.com>
2018-05-31 19:41:41 +0000

    Hack to work around use of long() for Python 3
    With this hack, hx711.py works in Python 3 without issues.

--- hx711.py
+++ hx711.py
@@ -1,7 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 import numpy  # sudo apt-get python-numpy
+import sys
 
+# Python 3 compatibility hack
+if sys.version_info > (3,):
+    long = int
 
 class HX711:
 
